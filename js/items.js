86470f97fa984088cce8fba4abf91310c86dd6cb
Leoooo-source <leobruneriepro@gmail.com>
2026-02-27 14:07:40 +0100

    Add files via upload

---
+++ js/items.js
@@ -0,0 +1,26 @@
+
+function renderInventory(){
+const list=document.getElementById("inventoryList");
+list.innerHTML="";
+player.inventory.forEach((item,i)=>{
+let btn=document.createElement("button");
+btn.className="button secondary";
+btn.innerText=item;
+btn.onclick=()=>useItem(i);
+list.appendChild(btn);
+});
+}
+
+function useItem(index){
+let item=player.inventory[index];
+if(item==="Potion"){
+let heal=Math.floor(player.maxHp*0.4);
+player.hp=Math.min(player.maxHp,player.hp+heal);
+}
+if(item==="Essence Mana"){
+player.mana=Math.min(player.maxMana,player.mana+30);
+}
+player.inventory.splice(index,1);
+save();
+location.reload();
+}
